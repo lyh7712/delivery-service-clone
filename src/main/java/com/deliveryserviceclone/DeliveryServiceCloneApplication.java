2743f74ac0548c59f53fa0fe7794bdd83a9240fb
lyh7712 <mmj2rv9@kakao.com>
2021-05-17 17:23:39 +0900

    Initial commit

---
+++ src/main/java/com/deliveryserviceclone/DeliveryServiceCloneApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryserviceclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryserviceclone/DeliveryServiceCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryserviceclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
